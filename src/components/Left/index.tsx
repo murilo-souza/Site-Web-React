7277c7e30c3c9d9d3916c43567cd5decaf7dc3c3
murilo-souza <murilo.l.souza232@gmail.com>
2021-07-19 22:35:42 -0300

    update Serviços
    Começando a pagina de serviços

--- src/components/Left/index.tsx
+++ src/components/Left/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Container, Title, Text } from './styles';
 
-import { Container } from './styles';
+interface Props{
+  title: string;
+  text: string;
+}
 
-export function Left(){
+
+export function Left({title, text}: Props){
   return (
     <Container>
-
+      <Title>{title}</Title>
+      <Text>{text}</Text>
     </Container>
   );
 }
--- src/components/Left/styles.ts
+++ src/components/Left/styles.ts
@@ -2,4 +2,17 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 
-`
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const Title = styled.h3`
+    font-size: 35px;
+`;
+
+export const Text = styled.p`
+    font-size: 25px
+`;

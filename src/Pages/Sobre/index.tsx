58ae882ecebf27ed204def9a6951ee4036fb487f
murilo-souza <murilo.l.souza232@gmail.com>
2021-07-22 22:02:37 -0300

    update

--- src/Pages/Sobre/index.tsx
+++ src/Pages/Sobre/index.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
 import { Container, Title} from './styles';
-import {Left} from '../../components/Left'
-import Imgee from '../../assets/cinza-escuro.png'
 
 export function Sobre(){
   return (
     <Container>
-        <Title>Sobre</Title>
-        <Left
-        title='hshshhshshshs'
-        text='ahhhhhhhhhhhdjjdu jucndjcwn jnjwncwjcnnccuwbwbuw j cuwucbwucnwucnwucnuc wj w wucuwc cwcuw '
-        Alt='Foto'
-        Img={Imgee}
-        />
+        <Title>Sobre</Title>  
     </Container>
   );
 }
--- src/Pages/Sobre/styles.ts
+++ src/Pages/Sobre/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    padding-top: 30px;
+    padding-top: 60px;
+    display: flex;
     align-items: center;
     justify-content: center;
 `;

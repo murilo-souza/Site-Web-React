09b5b04799bb4aa54ee8a9e28f2a95f34e237861
murilo-souza <murilo.l.souza232@gmail.com>
2021-07-24 23:51:26 -0300

    update geral

--- src/Pages/Sobre/index.tsx
+++ src/Pages/Sobre/index.tsx
@@ -12,6 +12,14 @@ export function Sobre(){
         <Image><img src={Img} alt="Foto sua" style={{width: "600px", height: "300px"}}/></Image>
         <Text>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto at sit saepe quos modi dolor quae eveniet quidem quam laboriosam consectetur quod et, id doloremque dolorem, necessitatibus nihil esse sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ea quam cupiditate reiciendis laborum enim perspiciatis, repudiandae maiores esse voluptatem animi expedita, magnam facere dolores facilis saepe aperiam, in harum.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, quia ut veniam suscipit exercitationem numquam deserunt, animi, est necessitatibus accusamus aspernatur. Ut minus voluptatibus necessitatibus distinctio natus, aspernatur sequi vitae.
+        </Text>
+      </Section>
+      <Section>
+        <Title>Sobre 2 <hr/></Title>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad a voluptatem, in ea beatae placeat commodi architecto omnis odit quas ratione necessitatibus assumenda error nostrum eius ab. Sed, dicta?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur rerum, perspiciatis corporis dolor excepturi praesentium reiciendis voluptas, ad in esse id! Provident minus eius beatae. Vero tempore voluptas repellat quis?
         </Text>
       </Section>
     </Container>
